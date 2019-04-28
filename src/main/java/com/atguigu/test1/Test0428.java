b46f6c6e65893fdff14d013c709471b89c22a29a
Max <maxwang.per@gmail.com>
2019-04-28 19:29:36 +0800

    jklj

--- src/main/java/com/atguigu/test1/Test0428.java
+++ src/main/java/com/atguigu/test1/Test0428.java
@@ -3,5 +3,6 @@ package com.atguigu.test1;
 public class Test0428 {
     public static void main(String[] args) {
         System.out.println("第一次使用idea操作GitHub！！！");
+        System.out.println("第二次使用idea操作GitHub！！！");
     }
 }
